0990609dac4bda43e08911f8024f556ad4e64407
Maksim <maks89855@gmail.com>
2023-08-24 05:46:16 +0300

    Added DbContext

--- DbContexts/ApplicationContext.cs
+++ DbContexts/ApplicationContext.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebCookingBook.Models;
 
 namespace WebCookingBook.DbContexts
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
