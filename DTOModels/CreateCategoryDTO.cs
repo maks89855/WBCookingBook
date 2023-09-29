f37fdb2652da736e340896c80f60fe5d2b253f42
Maksim Khlebov <maks89855@gmail.com>
2023-09-29 15:56:22 +0300

    Small changes

--- DTOModels/CreateCategoryDTO.cs
+++ DTOModels/CreateCategoryDTO.cs
@@ -9,6 +9,6 @@ namespace WebCookingBook.DTOModels
         [Required]
         [MaxLength(50)]
         public string NameCategory { get; set; } = "Category";
-        //public ICollection<Recipe> Recipes { get; set; } = new List<Recipe> { };
+        public ICollection<CreateRecipeDTO> Recipes { get; set; } = new List<CreateRecipeDTO> { };
     }
 }
